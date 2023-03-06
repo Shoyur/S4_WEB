f78605a17c0e693f27d232d5df8fb7f96bb4173c
Shoyur <100440995+Shoyur@users.noreply.github.com>
2023-03-05 19:38:52 -0500

    ...

--- Tuto-TS-Angular/04_Casting.ts
+++
@@ -1,7 +0,0 @@
-// Caster pour supposer un type :
-let test04_1: unknown = 'salut!';
-console.log((test04_1 as string).length);
-
-let test04_2: unknown = 56789231784314651378264;
-console.log((test04_2 as string).length);
-// console.log((test04_1 as string).length);
